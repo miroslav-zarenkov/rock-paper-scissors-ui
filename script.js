cbe2e7c82cc44e52b238b3420472471569eb69b0
miroslav-zarenkov <miroslav.zarenkov@gmail.com>
2021-06-03 18:38:56 +0300

    Add first UI prototype

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const computerChoice = ['rock', 'paper', 'scissors'];
 let computerSelection;
 let playerScore = 0;
 let computerScore = 0;
+let textResult;
 
 function computerPlay(){
     return computerChoice[Math.floor(Math.random() * computerChoice.length)];
@@ -10,71 +11,95 @@ function computerPlay(){
 
 function playRound(playerSelection, computerSelection){
     computerSelection = computerPlay();
-    playerSelection = prompt('Choose your destiny');
+    //playerSelection = prompt('Choose your destiny');
+    //playerSelection = "rock";
     if (computerSelection === 'rock'){
         if (playerSelection === null){
-            return(`Cancelled.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+            textResult = `Cancelled.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
         else if (playerSelection.toLowerCase() === 'rock'){
-           return(`Tie! Rock can\'t beat rock.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+           textResult = `Tie! Rock can\'t beat rock.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
        }
        else if (playerSelection.toLowerCase() === 'paper'){
         playerScore = ++playerScore;
-        return(`You win! Paper beats rock.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+        textResult = `You win! Paper beats rock.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
        }
        else if (playerSelection.toLowerCase() === 'scissors'){
         computerScore = ++computerScore;
-        return(`You lose! Rock beats scissors.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+        textResult = `You lose! Rock beats scissors.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
        }
        else{
-           return(`Only rock, paper and scissors are allowed!\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+        textResult = `Only rock, paper and scissors are allowed!\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
        }
     }   
     else if (computerSelection === 'paper'){
         if (playerSelection === null){
-            return(`Cancelled.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+            textResult = `Cancelled.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
         else if (playerSelection.toLowerCase() === 'rock'){
             computerScore = ++computerScore;
-            return(`You lose! Paper beats rock.\nThe score is: You - ${playerScore}, PC - ${computerScore}`); 
+            textResult = `You lose! Paper beats rock.\nThe score is: You - ${playerScore}, PC - ${computerScore}`; 
         }
         else if (playerSelection.toLowerCase() === 'paper'){
-            return(`Tie! Paper can\'t beat paper.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+            textResult = `Tie! Paper can\'t beat paper.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
         else if (playerSelection.toLowerCase() === 'scissors'){
             playerScore = ++playerScore;
-            return(`You win! Scissors beats paper.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+            textResult = `You win! Scissors beats paper.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
         else{
-            return(`Only rock, paper and scissors are allowed!\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+            textResult = `Only rock, paper and scissors are allowed!\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
     }
     else if (computerSelection === 'scissors'){
         if (playerSelection === null){
-            return(`Cancelled.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+            textResult = `Cancelled.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
         else if (playerSelection.toLowerCase() === 'rock'){
             playerScore = ++playerScore;
-            return(`You win! Rock beats scissors.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+            textResult = `You win! Rock beats scissors.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
         else if (playerSelection.toLowerCase() === 'paper'){
             computerScore = ++computerScore;
-            return(`You lose! Scissors beats paper.\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+            textResult = `You lose! Scissors beats paper.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
         else if (playerSelection.toLowerCase() === 'scissors'){
-            return(`Tie! Scissors can\'t beat scissors.\nThe score is: You - ${playerScore}, PC - ${computerScore}`)
+            textResult = `Tie! Scissors can\'t beat scissors.\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
         else{
-            return(`Only rock, paper and scissors are allowed!\nThe score is: You - ${playerScore}, PC - ${computerScore}`);
+            textResult = `Only rock, paper and scissors are allowed!\nThe score is: You - ${playerScore}, PC - ${computerScore}`;
         }
-    }    
+    } 
+    results.textContent = textResult;   
 }
 function game(){
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));
-console.log(playRound(playerSelection, computerSelection));  
+    //console.log(
+        playRound(playerSelection, computerSelection);
+        //alert(textResult); 
+}
+function setPlayerSelection(){
+    playerSelection = this.value;
 }
 
-game();
+const rockButton = document.querySelector('#rock');
+rockButton.addEventListener('click', setPlayerSelection);
+rockButton.addEventListener('click', game);
+const paperButton = document.querySelector('#paper');
+paperButton.addEventListener('click', setPlayerSelection);
+paperButton.addEventListener('click', game);
+const scissorsButton = document.querySelector('#scissors');
+scissorsButton.addEventListener('click', setPlayerSelection);
+scissorsButton.addEventListener('click', game);
+
+const results = document.querySelector(".results");
+
+
+// const resultsDiv = document.createElement('div');
+// resultsDiv.textContent = "WHAT";
+// document.body.appendChild(resultsDiv);
+
+//game();
+
+// function alertFunction(){
+//     alert('YAYAAYAYAY');
+// }
